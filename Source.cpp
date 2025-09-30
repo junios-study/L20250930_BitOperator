aff890091e6b3e6b7bd93ed8d93fef77d28e3338
junios-study <junios@naver.com>
2025-09-30 11:32:46 +0900

    쉬프트 연산자
    쉬프트 연산자

--- Source.cpp
+++ Source.cpp
@@ -3,27 +3,11 @@
 
 using namespace std;
 
-#define WorldStatic					0b00000001
-#define WorldDynamic				0b00000010
-#define Pawn						0b00000100
-#define PhysicsBody					0b00001000
-								//  0b00000100 or
-								//  0b00000100 1> 그럼 true
+
 int main()
 {
-	unsigned char MyActorType = Pawn | PhysicsBody;
-	string MyActorTypes = "Pawn, Physics";
-	if (MyActorType & Pawn)
-	if (MyActorType & Pawn)
-	{
-		cout << "나는 Pawn" << endl;
-	}
-	else
-	{
-		cout << "나는 Pawn 아님" << endl;
-
-	}
-
+	unsigned char Number = 0b00000001;
+	Number = Number << 1;
 
 	return 0;
 }
